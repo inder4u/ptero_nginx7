3f9ef1b2c21d308dabb27c062df3a4e7a115de0f
inder4u <getinder4u@gmail.com>
2022-12-04 11:44:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -h /home/container/
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
